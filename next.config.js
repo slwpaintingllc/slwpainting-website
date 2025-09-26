f966d78f004244655225ab66de08c9b901a215de
slwpaintingllc <slwpaintingllc@gmail.com>
2025-09-26 16:12:26 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,   // ✅ disable Image Optimization
-  },
-}
-module.exports = nextConfig
