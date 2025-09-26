6d8a54ef79d57626d7960eddc9483f92e0ce85bb
slwpaintingllc <slwpaintingllc@gmail.com>
2025-09-26 15:25:52 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true,   // ✅ disable Image Optimization
+  },
 }
 module.exports = nextConfig
